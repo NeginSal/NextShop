47b72e82c7d159727e18e3f049a779f0c789f615
NeginSal <neginsaljooghi@gmail.com>
2023-07-23 16:49:08 +0330

    setup project

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Eveniet delectus perspiciatis voluptas eligendi ab autem nulla quaerat id voluptatem nihil?
+        Praesentium ipsam reprehenderit corporis repellendus ad a nam assumenda sint?
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,14 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Suscipit similique error perferendis, quo est magni ipsam voluptatem repudiandae
+        laborum ad hic officiis accusantium,
+        placeat ducimus consequuntur fugit dolore quae corrupti?
+      </p>
+    </div>
+  );
+}
+
+export default TeamPage;
