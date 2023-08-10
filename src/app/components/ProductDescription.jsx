b3af53c06507f1376ee27edf399b2eda90776693
NeginSal <neginsaljooghi@gmail.com>
2023-08-10 17:06:07 +0330

    added:description component & suspense boundary

---
+++ src/app/components/ProductDescription.jsx
@@ -0,0 +1,19 @@
+async function fetchProductDescription(id) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await response.json();
+  return data;
+}
+const ProductDescription = async ({ id }) => {
+  const product = await fetchProductDescription(id);
+
+  return (
+    <div className="bg-blue-300">
+      <h1>product description</h1>
+      <p>{product.description}</p>
+    </div>
+  );
+}
+
+export default ProductDescription;
--- src/app/shop/products/[id]/page.jsx
+++ src/app/shop/products/[id]/page.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import Product from '../../../components/Product'
+import ProductDescription from "@/app/components/ProductDescription";
+
 
 const ProductPage = ({ params: { id } }) => {
   return (
     <div>
       <Link href='/shop/products' className="px-2 bg-green-800">Back to see all products</Link>
-      <Product id={id} />
+      <Suspense fallback={<div>Loading Product ...</div>}>
+        <Product id={id} />
+      </Suspense>
+      <Suspense fallback={<div>Loading Description ...</div>}>
+        <ProductDescription id={id} />
+      </Suspense>
+
     </div>
   );
 }
