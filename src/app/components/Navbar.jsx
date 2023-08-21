816ba05e8629786b552ec4ad395cb58f73419d71
NeginSal <neginsaljooghi@gmail.com>
2023-08-21 17:33:31 +0330

    added:material icons

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,13 +3,25 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className=" flex flex-col items-center sm:flex sm:flex-row sm:justify-between bg-sky-100 text-sky-700  text-xl p-3 sticky top-0">
-      <div className="text-3xl font-bold">
+      <div className="flex items-center text-3xl font-bold hover:text-sky-500">
         <Link href='/'>Chess Shop</Link>
+        <span class="material-symbols-outlined pl-1">
+          chess
+        </span>
       </div>
-      <div className="flex flex-col sm:flex sm:flex-row ">
-        <Link href='/about' className="m-2">About Us</Link>
-        <Link href='/about/team' className="m-2">Our Team</Link>
-        <Link href='/shop/products' className="m-2">Products</Link>
+      <div className="flex-col items-center sm:flex sm:flex-row  ">
+        <p class=" flex items-center mx-2 hover:text-sky-500">
+          <Link href='/about'>About Us</Link>
+          <i class="material-icons"> info</i>
+        </p>
+        <p class=" flex items-center mx-2 hover:text-sky-500">
+          <Link href='/about/team'>Our Team</Link>
+          <i class="material-icons">group</i>
+        </p>
+        <p class=" flex items-center mx-2 hover:text-sky-500">
+          <Link href='/shop/products'>Products</Link>
+          <i class="material-icons">shopping_cart</i>
+        </p>
       </div>
     </div>
   );
