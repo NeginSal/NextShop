b7e5562bf06180a3a586d20633b01d49a256eaa5
NeginSal <neginsaljooghi@gmail.com>
2023-08-22 17:12:07 +0330

    added:styles for products

--- src/app/shop/products/page.jsx
+++ src/app/shop/products/page.jsx
@@ -16,19 +16,20 @@ const ReposPage = async () => {
 
   console.log(products)
   return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <Link href={`/shop/products/${product.id}`}>
-              <p>{product.title}</p>
-              <p>{product.price}</p>
+    <div class="grid grid-cols-1 gap-1 mx-5 lg:grid lg:grid-cols-4 lg:gap-4 lg:m-8">
+      {
+        products.map((product) => (
+          <div key={product.id} class="flex flex-col items-center border border-purple-300 rounded-xl bg-lime-50">
+            <img src={product.image} className="w-52 h-52 m-5 rounded-sm " />
+            <p className="truncate my-3 text-purple-900 bg-purple-200 p-2 rounded-lg ">
+                {product.title}
+            </p>
+            <Link href={`/shop/products/${product.id}`} className="">
+              view details
             </Link>
-          </li>
-        ))}
-      </ul>
-
+          </div>
+        ))
+      }
     </div>
   );
 }
