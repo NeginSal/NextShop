0dacd4dac590d2afc434820766cf1d348affa059
NeginSal <neginsaljooghi@gmail.com>
2023-08-26 16:08:09 +0330

    edited:loading component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,11 @@
 const LoadingPage = () => {
-  return ( 
-    <div>
-      <h1>loading .... </h1>
+  return (
+    <div class="h-screen bg-white">
+      <div class="flex justify-center items-center h-full">
+        <img class="h-32 w-32" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default LoadingPage;
