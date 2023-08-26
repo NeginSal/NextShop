0b64e3874d1d063ae02c39b1311ac14971865eac
NeginSal <neginsaljooghi@gmail.com>
2023-08-26 15:49:15 +0330

    edited:product page style

--- src/app/shop/products/page.jsx
+++ src/app/shop/products/page.jsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+export const metadata = {
+  title: 'products'
+}
+
 async function fetchProducts() {
   const response = await fetch('https://fakestoreapi.com/products', {
     next: {
@@ -12,25 +16,29 @@ async function fetchProducts() {
 }
 
 const ReposPage = async () => {
+
   const products = await fetchProducts();
 
-  console.log(products)
   return (
-    <div class="grid grid-cols-1 gap-1 mx-5 lg:grid lg:grid-cols-4 lg:gap-4 lg:m-8">
-      {
-        products.map((product) => (
-          <div key={product.id} class="flex flex-col items-center border border-purple-300 rounded-xl bg-lime-50">
-            <img src={product.image} className="w-52 h-52 m-5 rounded-sm " />
-            <p className="truncate my-3 text-purple-900 bg-purple-200 p-2 rounded-lg ">
+    <>
+      <h1 className="text-center text-violet-800 text-5xl my-5">* PRODUCTS *</h1>
+      <div class="grid grid-cols-1 gap-1 mx-5 lg:grid lg:grid-cols-4 lg:gap-5 lg:m-8">
+        {
+          products.map((product) => (
+            <div key={product.id} class="p-3 shadow-md h-full border text-center border-purple-300 rounded-xl bg-lime-50">
+              <img src={product.image} className="w-52 h-52 mx-auto my-5 rounded-sm"/>
+              <p className="truncate my-3 text-purple-900 bg-purple-200 p-2 rounded-lg ">
                 {product.title}
-            </p>
-            <Link href={`/shop/products/${product.id}`} className="">
-              view details
-            </Link>
-          </div>
-        ))
-      }
-    </div>
+              </p>
+              <Link href={`/shop/products/${product.id}`} className="text-violet-950 hover:bg-violet-200 hover:p-2 hover:rounded-lg ">
+                view details
+              </Link>
+            </div>
+          ))
+        }
+      </div>
+    </>
+
   );
 }
 
